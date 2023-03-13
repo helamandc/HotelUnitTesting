a2cf49de98afc2dbd4dcaf63d4bf7aa9f47cbc1e
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-03-13 19:27:50 +1100

    Add project files.

---
+++ Hotel.UnitTests/ReservationTests.cs
@@ -0,0 +1,43 @@
+using HotelUnitTesting.Fundamentals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hotel.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnTrue()
+        {
+            //Arrange
+            Reservation myReservation = new Reservation();
+            //Act
+            var result = myReservation.CanBeCancelledBy(new User { IsAdmin = true});
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_MadeByUser_ReturnTrue()
+        {
+            //Arrange
+            var user = new User();
+            Reservation myReservation = new Reservation { MadeBy = user };
+            //Act
+            var result = myReservation.CanBeCancelledBy(user);
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_OtherUser_ReturnFalse()
+        {
+            //Arrange
+            Reservation myReservation = new Reservation();
+            //Act
+            var result = myReservation.CanBeCancelledBy(new User { IsAdmin = false });
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ Reservation.UnitTests/ReservationTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelUnitTesting.Fundamentals;
+
+namespace Reservation.UnitTests
+{
+    [TestClass]
+    public class ReservationTest
+    {
+        [TestMethod]
+        public void CanBecancelledBy_MadeByUser_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            //Act
+            //Assert
+
+        }
+    }
+}
